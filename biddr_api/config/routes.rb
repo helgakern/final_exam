ba41c565ece9623302a068e0fdf6e0a88fc1f426
helgakern <helgakern@gmail.com>
2020-06-08 10:28:56 -0700

    created models, controllers, routes and db

--- biddr_api/config/routes.rb
+++ biddr_api/config/routes.rb
@@ -1,3 +1,24 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # The namespace method in Rails routes makes it so it will automatically look in a directory api, then in a subdirectory v1 for QuestionsController.  
+  namespace :api, defaults: { format: :json } do
+    # /api..
+    namespace :v1 do
+      # /api/v1...
+      resources :auctions do
+        resources :bids, only: [:create, :destroy, :index, :show]
+        resources :publishings, only: :create
+        resources :reservings, only: :create
+      end
+      # /api/v1/auctions
+      resource :session, only: [:create, :destroy]
+      # /api/v1/users
+      resources :users, shallow: true, only: [:create, :new, :show, :index] do
+      # api/v1/users/current
+        get :current, on: :collection
+        # default
+        # api/v1/user/:id/current
+      end
+    end
+  end
 end
--- biddr_api/db/seeds.rb
+++ biddr_api/db/seeds.rb
@@ -5,3 +5,62 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Bid.delete_all
+Auction.delete_all
+User.delete_all
+
+super_user = User.create(
+  first_name: "John",
+  last_name: "Doe",
+  email: "johndoe@gmail.com",
+  password: "supersecret",
+  is_admin: true
+)
+
+5.times do
+  first_name = Faker::Name.first_name
+  last_name = Faker::Name.last_name
+  User.create(
+    first_name: first_name,
+    last_name: last_name,
+    email: "#{first_name.downcase}.#{last_name.downcase}@gmail.com",
+    password: "supersecret"
+  )
+end
+
+users = User.all
+
+5.times do
+  created_at = Faker::Date.backward(days: 10)
+  end_at = Faker::Date.forward(days: 30)
+
+  auction = Auction.create(
+
+    title: Faker::Hacker.say_something_smart,
+    description: Faker::ChuckNorris.fact,
+    reserve_price: rand(100_0),
+    aasm_state: :published,
+    end_date: end_at,
+    created_at: created_at,
+    updated_at: created_at,
+    user: users.sample
+  )
+  if auction.valid?
+    auction.bids = rand(0..10).times.map do
+      Bid.new(
+        amount: rand(0..300),
+        user: users.sample
+      )
+    end
+
+  end
+end
+
+auction = Auction.all
+
+
+puts Cowsay.say("Generated #{Auction.count} auctions", :dragon)
+puts Cowsay.say("Generated #{Bid.count} bids", :cow)
+puts Cowsay.say("Created #{users.count} users", :tux)
+puts "Login with #{super_user.email} and password of '123'"
